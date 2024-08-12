c63465b04f85c83c428ab4be6d076629ecf27a82
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-08-12 14:15:13 +0500

    fix: Fixed The Type For UseState

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [randomquote, setrandomquote] = useState("");
+  const [randomquote, setrandomquote] = useState<{
+    quote: string;
+    author: string;
+  }>();
   const GetQuote = async () => {
     return fetch(
       "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
@@ -13,18 +16,26 @@ function App() {
         return quotes;
       });
   };
-  GetQuote();
+  const getRandomQuote = async () => {
+    const quotes = await GetQuote();
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setrandomquote(quotes[randomIndex]);
+  };
 
-  // console.log(GetQuote());
-  const handlechange = async () => {};
   return (
     <>
       <div id="quote-box" className=" w-96 h-max bg-red-50">
         <div id="text" className="w-28 text-black">
-          {randomquote}
+          Heelo
+          {randomquote?.quote}
         </div>
         <div id="author"></div>
-        <button id="new-quote" onClick={handlechange}>
+        <button
+          id="new-quote"
+          onClick={async () => {
+            await getRandomQuote();
+          }}
+        >
           New Quote
         </button>
         <a id="tweet-quote"></a>
