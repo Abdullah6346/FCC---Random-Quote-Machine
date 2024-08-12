33ccf73691db95b459a114010c58d5f099ef98dc
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-08-12 12:37:17 +0500

    fix: Fetch APi Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const handlechange = async () => {
-    try {
-      const response = await fetch(
-        "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
-      );
-      const quotes = await response.json();
-      console.log(quotes.quotes);
-    } catch (error) {
-      return `Error Parsing DATA 205 ${error}`;
-    }
+  const [randomquote, setrandomquote] = useState("");
+  const GetQuote = async () => {
+    fetch(
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    )
+      .then((response) => response.json()) //2
+      .then((quote) => {
+        console.log(quote.quotes); //3
+      });
   };
+  console.log(GetQuote());
+  const handlechange = async () => {};
   return (
     <>
       <div id="quote-box" className=" w-96 h-max bg-red-50">
         <div id="text" className="w-28 text-black">
-          HEY I am THE Quote
+          {randomquote}
         </div>
         <div id="author"></div>
         <button id="new-quote" onClick={handlechange}>
@@ -29,4 +31,3 @@ function App() {
 }
 
 export default App;
-// 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
